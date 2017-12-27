c852968a416821c2ca738372d6de06a0f18f904d
Mentor Elshani <mentorelshani04@gmail.com>
2017-12-27 14:15:19 +0100

    Elevator and entry update functions

--- app/Http/Controllers/ElevatorController.php
+++ app/Http/Controllers/ElevatorController.php
@@ -11,10 +11,16 @@ class ElevatorController extends Controller
         return Elevator::where('entry_id',$entry_id)->get();
     }
 
+    public function getElevator($id){
+        return Elevator::whereId($id)->first(
+
+        );
+    }
+
     public function add(Request $request){
 
         $this->validate($request,[
-            'entry_id' => 'required|integer',
+            'entry_id' => 'exists:entries,id',
             'identifier' => 'required'
         ]);
 
@@ -33,5 +39,42 @@ class ElevatorController extends Controller
         $elevator->save();
 
         return $elevator;
+    }
+
+    public function update(Request $request){
+
+        $this->validate($request,[
+            'id' => 'exists:elevators',
+            'entry_id' => 'exists:entries,id',
+            'identifier' => 'required'
+        ]);
+
+        $id = $request->id;
+        $entry_id = $request->entry_id;
+        $identifier = $request->identifier;
+        $type = $request->type;
+        $made_in = $request->made_in;
+        $company = $request->company;
+
+        $elevator = Elevator::whereId($id)->first();
+        $elevator->entry_id = $entry_id;
+        $elevator->identifier = $identifier;
+        $elevator->type = $type;
+        $elevator->made_in = $made_in;
+        $elevator->company = $company;
+        $elevator->update();
+
+        return $elevator;
+    }
+
+    public function destroy($id){
+
+        $a = Elevator::find($id);
+
+        if ($a != null) {
+            $a->delete();
+            return "u fshi";
+        }
+        return "Nuk ekziston";
     }
 }
