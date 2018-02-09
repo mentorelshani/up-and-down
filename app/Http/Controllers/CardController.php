1cfb727685abd651107b0352696d99b1ad878481
Mentor Elshani <mentorelshani04@gmail.com>
2018-02-09 11:00:35 +0100

    Validation bug fix

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -46,32 +46,19 @@ class CardController extends Controller
 
     public function getCardAccess($card_id){
 
-         $relay_ids = Card_access::where('card_id', $card_id)->select(['relay_id'])->get();
-         $access_point_ids = Relay::whereIn('id', $relay_ids)->select(['access_point_id'])->get();
-         $elevator_ids = Access_point::whereIn('id', $access_point_ids)->select(['elevator_id'])->get();
-         $entry_ids = Elevator::whereIn('id', $elevator_ids)->select(['entry_id'])->get();
-         $building_ids = Entry::whereIn('id', $entry_ids)->select(['building_id'])->get();
-
-
-         $buildings = Building::whereIn('id', $building_ids)->with(['entries' => function ($query) use($entry_ids, $elevator_ids, $access_point_ids, $relay_ids){
-             $query->whereIn('id', $entry_ids)->with(['elevators' => function ($query) use ($elevator_ids, $access_point_ids, $relay_ids){
-                 $query->whereIn('id', $elevator_ids)->with(['access_points' => function($query) use ($access_point_ids, $relay_ids){
-                     $query->whereIn('id', $access_point_ids)->with(['relays' => function($query) use ($relay_ids){
-                         $query->whereIn('id', $relay_ids);
-                     }]);
-                 }]);
-             }]);
-         }])->get();
-
-         return $buildings;
+        return Card_access::where('card_id',$card_id)->with('relay.access_point.elevator.entry.building.address.city')->get();
     }
 
     public function giveAccess(giveAccessToCardRequest $request){
 
-        $card_access = new Card_access();
-        $card_access->card_id = $request->card_id;
-        $card_access->relay_id = $request->relay_id;
-        $card_access->save();
+        foreach ($request->relay_ids as $relay_id){
+            $card_access = new Card_access();
+            $card_access->card_id = $request->card_id;
+            $card_access->relay_id = $relay_id;
+            $card_access->save();
+        }
+        return $request->relay_ids;
+
     }
 
     public function deleteAccess($card_id, $relay_id){
