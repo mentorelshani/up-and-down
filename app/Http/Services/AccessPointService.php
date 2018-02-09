f9fa9220937a5b7100d8d43a8ca793aecedc1167
TriumfM <triumf_96@hotmail.com>
2018-02-09 09:57:25 +0100

    IMEI fix

--- app/Http/Services/AccessPointService.php
+++ app/Http/Services/AccessPointService.php
@@ -21,7 +21,7 @@ class AccessPointService
     {
         $access_point->elevator_id =$request->elevator_id;
         $access_point->version_id = $request->version_id;
-        $access_point->imei = $request->IMEI;
+        $access_point->imei = $request->imei;
         $access_point->phone_number = $request->phone_number;
         $access_point->notes = $request->notes;
         $access_point->active = $request->active;
